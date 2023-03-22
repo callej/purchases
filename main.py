bbe93c74b02e091dcd2df18f50f8161a132d54ed
Calle Johansson <callej@live.com>
2023-03-22 12:06:00 -0700

    A new updated version of the purchase database.
    This version includes the quantity of each item being purchased.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from tkinter import ttk, messagebox
 
 
 class App:
-    def __init__(self, root1):
-        self.root = root1
+    def __init__(self, root):
+        self.root = root
         self.root.title("Database App")
 
         self.create_tables()
@@ -54,8 +54,17 @@ class App:
         CREATE TABLE IF NOT EXISTS purchases (
             id INTEGER PRIMARY KEY,
             person_id INTEGER NOT NULL,
+            FOREIGN KEY (person_id) REFERENCES people (id)
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS purchase_items (
+            id INTEGER PRIMARY KEY,
+            purchase_id INTEGER NOT NULL,
             item_id INTEGER NOT NULL,
-            FOREIGN KEY (person_id) REFERENCES people (id),
+            quantity INTEGER NOT NULL,
+            FOREIGN KEY (purchase_id) REFERENCES purchases (id),
             FOREIGN KEY (item_id) REFERENCES items (id)
         )
         """)
@@ -94,15 +103,14 @@ class App:
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute("INSERT INTO people (name, email, phone) VALUES (?, ?, ?)", (name        , email, phone))
+        cursor.execute("INSERT INTO people (name, email, phone) VALUES (?, ?, ?)", (name, email, phone))
         conn.commit()
         conn.close()
 
+        messagebox.showinfo("Success", "Person added successfully")
         self.name_entry.delete(0, tk.END)
         self.email_entry.delete(0, tk.END)
         self.phone_entry.delete(0, tk.END)
-
-        messagebox.showinfo("Success", "Person added successfully")
 
     # Items Tab
     def create_items_tab(self):
@@ -131,7 +139,7 @@ class App:
         try:
             price = float(price)
         except ValueError:
-            messagebox.showerror("Error", "Invalid price value")
+            messagebox.showerror("Error", "Invalid price format")
             return
 
         conn = sqlite3.connect("database.db")
@@ -141,110 +149,129 @@ class App:
         conn.commit()
         conn.close()
 
+        messagebox.showinfo("Success", "Item added successfully")
         self.item_name_entry.delete(0, tk.END)
         self.price_entry.delete(0, tk.END)
-
-        messagebox.showinfo("Success", "Item added successfully")
 
     # Purchases Tab
     def create_purchases_tab(self):
         # Labels
         ttk.Label(self.purchases_tab, text="Person:").grid(column=0, row=0, padx=5, pady=5, sticky=tk.W)
         ttk.Label(self.purchases_tab, text="Item:").grid(column=0, row=1, padx=5, pady=5, sticky=tk.W)
+        ttk.Label(self.purchases_tab, text="Quantity:").grid(column=0, row=2, padx=5, pady=5, sticky=tk.W)
 
-        # Dropdowns
-        self.person_var = tk.StringVar()
-        self.item_var = tk.StringVar()
+        # Combobox widgets
+        self.person_combobox = ttk.Combobox(self.purchases_tab, postcommand=self.update_people_combobox, state="readonly")
+        self.item_combobox = ttk.Combobox(self.purchases_tab, postcommand=self.update_items_combobox, state="readonly")
+        self.person_combobox.grid(column=1, row=0, padx=5, pady=5)
+        self.item_combobox.grid(column=1, row=1, padx=5, pady=5)
 
-        self.person_dropdown = ttk.Combobox(self.purchases_tab, textvariable=self.person_var, postcommand=self.update_people_dropdown)
-        self.item_dropdown = ttk.Combobox(self.purchases_tab, textvariable=self.item_var, postcommand=self.update_items_dropdown)
-
-        self.person_dropdown.grid(column=1, row=0, padx=5, pady=5)
-        self.item_dropdown.grid(column=1, row=1, padx=5, pady=5)
+        # Entry widgets
+        self.quantity_entry = ttk.Entry(self.purchases_tab)
+        self.quantity_entry.grid(column=1, row=2, padx=5, pady=5)
 
         # Add button
-        ttk.Button(self.purchases_tab, text="Add Purchase", command=self.add_purchase).grid(column=1, row=2, padx=5, pady=5)
+        ttk.Button(self.purchases_tab, text="Add Purchase", command=self.add_purchase).grid(column=1, row=3, padx=5, pady=5)
 
-    def update_people_dropdown(self):
+    def update_people_combobox(self):
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute("SELECT name FROM people")
-        people = [person[0] for person in cursor.fetchall()]
-
+        cursor.execute("SELECT id, name FROM people")
+        people = cursor.fetchall()
         conn.close()
 
-        self.person_dropdown["values"] = people
+        self.person_combobox["values"] = [f"{person[0]} - {person[1]}" for person in people]
 
-    def update_items_dropdown(self):
+    def update_items_combobox(self):
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute("SELECT item_name FROM items")
-        items = [item[0] for item in cursor.fetchall()]
-
+        cursor.execute("SELECT id, item_name FROM items")
+        items = cursor.fetchall()
         conn.close()
 
-        self.item_dropdown["values"] = items
+        self.item_combobox["values"] = [f"{item[0]} - {item[1]}" for item in items]
 
     def add_purchase(self):
-        person_name = self.person_var.get()
-        item_name = self.item_var.get()
+        person = self.person_combobox.get()
+        item = self.item_combobox.get()
+        quantity = self.quantity_entry.get()
 
-        if not person_name or not item_name:
-            messagebox.showerror("Error", "Please select a person and an item")
+        if not person or not item or not quantity:
+            messagebox.showerror("Error", "Please fill in all fields")
             return
+
+        try:
+            quantity = int(quantity)
+            if quantity < 1:
+                raise ValueError
+        except ValueError:
+            messagebox.showerror("Error", "Invalid quantity")
+            return
+
+        person_id = int(person.split(" ")[0])
+        item_id = int(item.split(" ")[0])
 
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute("SELECT id FROM people WHERE name = ?", (person_name,))
-        person_id = cursor.fetchone()[0]
+        cursor.execute("INSERT INTO purchases (person_id) VALUES (?)", (person_id,))
+        purchase_id = cursor.lastrowid
+        cursor.execute("INSERT INTO purchase_items (purchase_id, item_id, quantity) VALUES (?, ?, ?)", (purchase_id, item_id, quantity))
 
-        cursor.execute("SELECT id FROM items WHERE item_name = ?", (item_name,))
-        item_id = cursor.fetchone()[0]
-
-        cursor.execute("INSERT INTO purchases (person_id, item_id) VALUES (?, ?)", (person_id, item_id))
         conn.commit()
         conn.close()
 
         messagebox.showinfo("Success", "Purchase added successfully")
+        self.person_combobox.set("")
+        self.item_combobox.set("")
+        self.quantity_entry.delete(0, tk.END)
 
-        # Report Tab
-
+    # Report Tab
     def create_report_tab(self):
-        self.report_text = tk.Text(self.report_tab, wrap=tk.WORD, width=80, height=20)
+        self.report_text = tk.Text(self.report_tab, wrap=tk.WORD, state="disabled", width=80, height=20)
         self.report_text.grid(column=0, row=0, padx=5, pady=5)
 
-        ttk.Button(self.report_tab, text="Show Report", command=self.show_report).grid(column=0, row=1, padx=5, pady=5)
+        ttk.Button(self.report_tab, text="Generate Report", command=self.generate_report).grid(column=0, row=1, padx=5, pady=5)
 
-    def show_report(self):
+    def generate_report(self):
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
         cursor.execute("""
-                SELECT people.name, items.item_name, items.price
-                FROM purchases
-                INNER JOIN people ON purchases.person_id = people.id
-                INNER JOIN items ON purchases.item_id = items.id
-                """)
+        SELECT p.id, pe.name, i.item_name, pi.quantity, i.price
+        FROM purchases p
+        JOIN people pe ON p.person_id = pe.id
+        JOIN purchase_items pi ON p.id = pi.purchase_id
+        JOIN items i ON pi.item_id = i.id
+        """)
         purchases = cursor.fetchall()
+
+        report = "Purchases:\n\n"
+
+        total_cost = 0
+        for purchase in purchases:
+            cost = purchase[3] * purchase[4]
+            report += f"{purchase[1]} bought {purchase[3]} x {purchase[2]} for ${cost:.2f}\n"
+            total_cost += cost
+
+        report += f"\nTotal Cost: ${total_cost:.2f}"
 
         conn.close()
 
+        self.report_text.configure(state="normal")
         self.report_text.delete(1.0, tk.END)
-        total_cost = 0
-
-        for name, item_name, price in purchases:
-            self.report_text.insert(tk.END, f"{name} bought {item_name} for ${price:.2f}\n")
-            total_cost += price
-
-        self.report_text.insert(tk.END, f"\nTotal cost of all purchases: ${total_cost:.2f}")
+        self.report_text.insert(tk.END, report)
+        self.report_text.configure(state="disabled")
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
     root.mainloop()
+
+
+
 
 
